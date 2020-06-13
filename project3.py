5e6624fc80049311870c27d5447e198075e0dd24
Frank <usfrank02@gmail.com>
2020-06-12 20:19:24 -0700

    python application3

---
+++ project3.py
@@ -0,0 +1,5 @@
+name = input("Enter your name:")
+print (name)
+year_of_born = int(input("What's the year you were born?"))
+age = 2020 - year_of_born
+print("You will be {} years old this year!".format(age))
