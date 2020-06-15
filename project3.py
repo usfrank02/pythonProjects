9e7bd047db91125ddb36225ff84e5ce2cbac52c3
Frank Ushizimpumu <usfrank02@gmail.com>
2020-06-14 20:15:30 -0700

    comment for the calculation of age

--- project3.py
+++ project3.py
@@ -6,5 +6,7 @@ print (name)
 
 #Request user to input the year he was born
 year_of_born = int(input("What's the year you were born?"))
+
+#Calculate the age of the user
 age = 2020 - year_of_born
 print("You will be {} years old this year!".format(age))
