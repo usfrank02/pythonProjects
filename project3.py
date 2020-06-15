f33703b1107978ceaa6f04a0a799558b0d63127d
Frank Ushizimpumu <usfrank02@gmail.com>
2020-06-14 19:57:37 -0700

    Comment added on the project2

--- project3.py
+++ project3.py
@@ -3,6 +3,8 @@ import sys
 
 name = input("Enter your name:")
 print (name)
+
+#Request user to input the year he was born
 year_of_born = int(input("What's the year you were born?"))
 age = 2020 - year_of_born
 print("You will be {} years old this year!".format(age))
